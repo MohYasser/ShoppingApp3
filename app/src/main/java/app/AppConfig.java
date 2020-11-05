f2a5f2c6c1dd3956e82308ee1b5477acbb3f8386
MohYasser <mohamedyasser-99@hotmail.com>
2020-11-05 10:38:42 +0200

    All clear

--- app/src/main/java/app/AppConfig.java
+++ app/src/main/java/app/AppConfig.java
@@ -3,9 +3,9 @@ package app;
 public class AppConfig {
     // Server user login url
 
-    public static String URL_LOGIN = "http://192.168.1.7/android_login_api/login.php";
+    public static String URL_LOGIN = "http://192.168.43.121/android_login_api/login.php";
 
     // Server user register url
-    public static String URL_REGISTER = "http://192.168.1.7/android_login_api/register.php";
+    public static String URL_REGISTER = "http://192.168.43.121/android_login_api/register.php";
 
 }
